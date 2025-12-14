01ce20d69b0609253b1f473f19dde2151ffaee27
swastika <royswas2004@gmail.com>
2025-12-14 18:57:01 +0530

    msg

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,9 +1,9 @@
 
-const Category = require("../models/category.model");
+const Category = require("../models/categories.model");
 exports.craeteCategory=async(req,res)=>{
     try{
         const category=await Category.create(req.body)
-        res.status(201).json(category,{message:"Category created successfully",status_code:201})
+        res.status(201).json({cate:category,message:"Category created successfully",status_code:201})
 
     }catch(error){
         res.status(400).json({ message: error.message });
@@ -15,7 +15,7 @@ exports.getAllCategory=async(req,res)=>{
     try{
         const allcategory=await Category.find()
         if(allcategory){
-            res.status(200).json(allcategory,{message:"Category fetch successfully",status_code:200})
+            res.status(200).json({categories:allcategory,message:"Category fetch successfully",status_code:200})
         }
         else{
             res.status(400).json({message:"category not found",status_code:400})
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const {
-  createCategory,
+  craeteCategory,
   getAllCategory
 } = require("../controllers/category.controller");
 
 const router = express.Router();
 
-router.post("/create-category", createCategory);
+router.post("/create-category", craeteCategory);
 router.get("/getAllCategory",getAllCategory)
 
 module.exports = router;
