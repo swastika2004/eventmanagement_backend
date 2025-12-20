4cb33052564dba6fa998b4a379f48ff387bb01e0
anikhet <anikhetmaity800@gmail.com>
2025-12-20 18:18:02 +0530

    msg

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -9,3 +9,58 @@ export const createevent=async(req,res)=>{
     };
     
 }
+exports.getAllEvent=async(req,res)=>{
+    try{
+        const allevent=await Event.find()
+        if(all){
+            res.status(200).json({events:allevent,message:"Event fetch successfully",status_code:200})
+        }
+        else{
+            res.status(400).json({message:"Event not found",status_code:400})
+        }
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+}
+
+exports.updateEvent=async(req,res)=>{
+  try{
+    const updatedEventData=await Event.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedEventData) {
+      return res.status(404).json({
+        message: "event not found",
+      });
+    }
+    res.status(200).json({
+      category: updatedEventData,
+      message: "event updated successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error updating event",
+      error: error.message,
+    });
+  } 
+};
+exports.deleteEvent = async (req, res) => {
+  try {
+    const deletedEvent = await Category.findByIdAndDelete(req.params.id);
+    if (!deletedEvent) {
+      return res.status(404).json({
+        message: "Event not found",
+      });
+    }
+    res.status(200).json({
+      message: "Event deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error deleting Event",
+      error: error.message,
+    });
+  }
+};
--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -4,3 +4,10 @@ const router=express.Router()
  router.post('/create-event',createevent)
 
  module.exports = router;
+
+ router.post("/create-event", createevent);
+ router.get("/getAllEvent",getAllEvent);
+ router.put("/updateEvent/:id", updateEvent);
+ router.delete("/deleteEvent/:id", deleteEvent);
+ 
+ module.exports = router;
